af7d074bf94adadbde6c7020162669351ed10f58
Brian Jayson P. Umandap <brian.umandap@dotrgov.ph>
2024-08-15 08:42:25 +0800

    type of work tbl

--- atdb/urls.py
+++ atdb/urls.py
@@ -4,4 +4,5 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
 	path('', login_required(views.atdb), name = 'atdb'),
+	path('towtbl/', login_required(views.towtbl), name = 'towtbl'),
 ]
--- atdb/views.py
+++ atdb/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import JsonResponse, HttpResponse
+from .models import Typeofwork
 
 
 # Create your views here.
@@ -9,3 +10,9 @@ def atdb(request):
 		
 	}
 	return render(request, 'atdb/atdb.html', query)
+
+def towtbl(request):
+	query = {
+		'tow': Typeofwork.objects.all(),
+	}
+	return render(request, 'atdb/towtbl.html', query)
