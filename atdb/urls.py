2b84f1e7d951d82ade733d894fd32106ddbaa943
Brian Jayson P. Umandap <brian.umandap@dotrgov.ph>
2024-08-15 13:47:18 +0800

    created listview and converted to class based

--- atdb/urls.py
+++ atdb/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from .views import ATDBView, TowTblView, RTView, ATView, RSView, CLView, FSView, MView
 
 urlpatterns = [
-	path('', login_required(views.atdb), name = 'atdb'),
-	path('towtbl/', login_required(views.towtbl), name = 'towtbl'),
+    path('', staff_member_required(ATDBView.as_view()), name='atdb'),
+    path('towtbl/', staff_member_required(TowTblView.as_view()), name='towtbl'),
+    path('rt/', staff_member_required(RTView.as_view()), name='rt'),
+    path('at/', staff_member_required(ATView.as_view()), name='at'),
+    path('rs/', staff_member_required(RSView.as_view()), name='rs'),
+    path('cl/', staff_member_required(CLView.as_view()), name='cl'),
+    path('fs/', staff_member_required(FSView.as_view()), name='fs'),
+    path('maindb/', staff_member_required(MView.as_view()), name='maindb'),
 ]
--- atdb/views.py
+++ atdb/views.py
@@ -1,18 +1,52 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import JsonResponse, HttpResponse
-from .models import Typeofwork
+from .models import Typeofwork, Region, BikeArea, RoadSection, BikeClass, FundSource, bikelanetbl
+from django.views.generic import TemplateView, ListView
 
 
 # Create your views here.
-def atdb(request):
-	query = {
-		
-	}
-	return render(request, 'atdb/atdb.html', query)
+class ATDBView(TemplateView):
+    template_name = 'atdb/atdb.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            # Add any additional context if needed
+        })
+        return context
 
-def towtbl(request):
-	query = {
-		'tow': Typeofwork.objects.all(),
-	}
-	return render(request, 'atdb/towtbl.html', query)
+class TowTblView(ListView):
+    model = Typeofwork
+    template_name = 'atdb/towtbl.html'
+    context_object_name = 'tow'
+
+class RTView(ListView):
+    model = Region
+    template_name = 'atdb/rt.html'
+    context_object_name = 'rt'
+
+class ATView(ListView):
+    model = BikeArea
+    template_name = 'atdb/at.html'
+    context_object_name = 'at'
+
+class RSView(ListView):
+    model = RoadSection
+    template_name = 'atdb/rs.html'
+    context_object_name = 'rs'
+
+class CLView(ListView):
+    model = BikeClass
+    template_name = 'atdb/cl.html'
+    context_object_name = 'cl'
+
+class FSView(ListView):
+    model = FundSource
+    template_name = 'atdb/fs.html'
+    context_object_name = 'fs'
+
+class MView(ListView):
+    model = bikelanetbl
+    template_name = 'atdb/maindb.html'
+    context_object_name = 'maindb'
